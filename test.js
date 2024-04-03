ed7561c671472ce35fd6a227ce0bd78f98ff51f7
sashok1987nik <sashok1987nk@ukr.net>
2024-04-03 18:24:31 +0200

    up

--- test.js
+++ test.js
@@ -1,2 +1,22 @@
+const box = document.querySelector('.js-box')
+const titleTimer = document.querySelector('.js-timer')
+let counter = 10;
 
+
+setTimeout(() => {
+    box.style.display = "block";
+    const id = setInterval(() => {
+        
+        titleTimer.textContent = counter;
+        counter -=1;
+        if (!counter) {
+            clearInterval(id);
+            titleTimer.textContent = 'X';
+            titleTimer.addEventListener("click", (() => {
+                box.style.display = "none";
+            }));
+            
+        }
+    },1000)
+}, 10000);
 
